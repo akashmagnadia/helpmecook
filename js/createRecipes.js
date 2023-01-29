4ae70129c28440e3a2d6e20fec2b4c7d60029e1a
Akash Magnadia <akash.magnadia@gmail.com>
2023-01-29 16:11:32 -0600

    Automated building of the recipe with instructions and ingredients from text file

--- js/createRecipes.js
+++ js/createRecipes.js
@@ -20,10 +20,32 @@ async function getTileDesc(path) {
         if (i === 0)
         { title += arr[i]; }
         else
-        { desc += arr[i]; }
+        {
+            if (arr[i].slice(-1) !== "") {
+                desc += arr[i] + " ";
+            } else {
+                desc += arr[i];
+            }
+
+        }
     }
 
     return { title, desc };
+}
+
+async function getTextLines(path) {
+    let text = await getTextFile(path);
+    let arr = text.split(/\r?\n/);
+
+    let linesToAdd = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length !== 0) {
+            linesToAdd.push('<li>' + arr[i] + '</li>');
+        }
+    }
+
+    return linesToAdd;
 }
 
 async function generateHTML_SpicesCard() {
@@ -41,14 +63,39 @@ async function generateHTML_SpicesCard() {
 
         //TODO: Fix this with new setup
         linesToAdd.push('<div class="container d-col d-wrap float" id="recipe_' + title.replace(/ /g,'') + '">');
+        linesToAdd.push('<div class="accordion">');
         linesToAdd.push('<div class="container d-row d-nowrap">');
         linesToAdd.push('<img class="floatingImg" src="' + imgPath + '" alt="..."/>');
-        linesToAdd.push('<div class="container d-col d-wrap">');
+        linesToAdd.push('<div class="container d-col d-wrap" style="position: relative">');
+        linesToAdd.push('<div class="down_arrow">&#8659;</div>');
+        linesToAdd.push('<div class="up_arrow">&#8657;</div>');
         linesToAdd.push('<h3 style="align-self: unset; margin-top: 15px;"> ' + title + ' </h3>');
         linesToAdd.push('<p style="margin: 5px;">');
         linesToAdd.push(description);
         linesToAdd.push('</p> </div> </div> </div>');
 
+        linesToAdd.push('<div class="ingredients_instructions">');
+        linesToAdd.push('<b class="ingredients_title"> Things you will need </b>');
+        linesToAdd.push('<ul class="ingredients_in_instructions">');
+        // add ingredients for a recipe
+        let ingredientsToPush = await getTextLines('information/recipe/recipe_' + i + '/ingredients.txt')
+        for (let j = 0; j < ingredientsToPush.length; j++) {
+            linesToAdd.push(ingredientsToPush[j]);
+        }
+        linesToAdd.push('</ul>');
+
+        linesToAdd.push('<b class="instructions_title"> Instructions </b>');
+        linesToAdd.push('<ol class="instructions">');
+        // add instructions for a recipe
+        let instructionsToPush = await getTextLines('information/recipe/recipe_' + i + '/instructions.txt')
+        for (let j = 0; j < instructionsToPush.length; j++) {
+            linesToAdd.push(instructionsToPush[j]);
+        }
+        linesToAdd.push('</ol>');
+        linesToAdd.push('</div> </div>');
+
         addHTMLLinesToCodeScreen(document.getElementById("recipe_list"), linesToAdd);
     }
+
+    build_accordion_listeners()
 }
--- js/recipe_instructions.js
+++ js/recipe_instructions.js
@@ -1,40 +1,38 @@
-let fullFontSize = window.getComputedStyle(document.getElementsByClassName("up_arrow")[0]).fontSize;
-let zeroFontSize = window.getComputedStyle(document.getElementsByClassName("down_arrow")[0]).fontSize;
+function build_accordion_listeners() {
+    let fullFontSize = window.getComputedStyle(document.getElementsByClassName("up_arrow")[0]).fontSize;
+    let zeroFontSize = window.getComputedStyle(document.getElementsByClassName("down_arrow")[0]).fontSize;
 
-const acc = document.getElementsByClassName("accordion");
-let i;
+    const acc = document.getElementsByClassName("accordion");
+    let i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("accordion_active");
-        const panel = this.nextElementSibling;
+    console.log(acc.length);
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("accordion_active");
+            const panel = this.nextElementSibling;
 
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                panel.style.marginTop = "0px";
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.style.marginTop = "10px";
+            }
 
-        //toggle up and down arrow
-        let upArrow = this.getElementsByClassName("up_arrow")[0];
-        let downArrow = document.getElementsByClassName("down_arrow")[0];
+            //toggle up and down arrow
+            let upArrow = this.getElementsByClassName("up_arrow")[0];
+            let downArrow = this.getElementsByClassName("down_arrow")[0];
 
-        let upArrowFontSize = window.getComputedStyle(upArrow).fontSize;
-        let downArrowFontSize = window.getComputedStyle(downArrow).fontSize;
+            let upArrowFontSize = window.getComputedStyle(upArrow).fontSize;
 
-        if (upArrowFontSize === "0px") {
-            upArrow.style.fontSize = fullFontSize;
-        } else {
-            upArrow.style.fontSize = zeroFontSize;
-        }
-
-        if (downArrowFontSize === "0px") {
-            downArrow.style.fontSize = fullFontSize;
-        } else {
-            downArrow.style.fontSize = zeroFontSize;
-        }
-
-    });
-
+            if (upArrowFontSize === "0px") {
+                upArrow.style.fontSize = fullFontSize;
+                downArrow.style.fontSize = zeroFontSize;
+            } else {
+                upArrow.style.fontSize = zeroFontSize;
+                downArrow.style.fontSize = fullFontSize;
+            }
+        });
+    }
 
 }
